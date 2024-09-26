cf7d1082a604d89890c4575cb86ed5e011657de3
Muhammad Ibrahim <theibraheemmuhammad@gmail.com>
2024-09-26 19:19:32 +0500

    Update Q5.c

--- Lab05/Q5.c
+++ Lab05/Q5.c
@@ -1,39 +1,20 @@
-/*Write a program that asks for the number of calories and fat grams in a food.  The program
-should display the percentage of calories that come from fat
-One gram of fat has 9 calories, so Calories from fat = fat grams * 9
-\Calories = fat grams * 9
-The percentage of calories from fat can be calculated as: calories from fat/total calories
-Input validation: Make sure the number of calories and fat grams are not less than 0.
-Also, the number of calories from fat cannot be greater than the total number of calories.  If that
-happens, display an error message indicating that either the calories or fat grams were incorrectly
-entered.*/
-
 #include<stdio.h>
-int main(){
-	float calories, fat, caloriesFromFat;
-	printf("Enter Calories: ");
-	scanf("%f", &calories);
-	
-	printf("Enter Fat(in grams): ");
-	scanf("%f", &fat);
-	
-	if (calories<0){
-		printf("Calories are entered as a negative number");
-		return 0;
-	}
-	else if(fat<0){
-		printf("Fat value is entered as a negative number");
-		return 0;
-	}
-	
-	caloriesFromFat = (fat*9);
-	if (caloriesFromFat > calories){
-		printf("The value of Fat or calories is not correct");
-	}
-	
-	float percentCalories = caloriesFromFat/calories;
-	
-	printf("The percentage of calories from fat are %.3lf%",percentCalories);
-	
-	
+void main(){
+    float gpa;
+    printf("Enter GPA: ");    
+    scanf("%f", &gpa);
+    
+    ((gpa>=0.0) && (gpa<1.0)) ?
+        printf("Semester Failed(Registration Suspended)"):
+        ((gpa>=1) && (gpa<2)) ?
+        printf("On Probation"):
+        ((gpa>=2) && (gpa<3))?
+        printf("no msg"):
+        ((gpa>=3) && (gpa<3.5))?
+        printf("Dean's list"):
+        ((gpa>=3.5) && (gpa<4))?
+        printf("highest honors for semester"):
+        printf("gpa not correct");
+        
+    
 }
