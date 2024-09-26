d40eeaddccd7e0499a38ac7caefc1252db9c3bfa
Muhammad Ibrahim <theibraheemmuhammad@gmail.com>
2024-09-26 19:48:27 +0500

    Update Q2.c

--- Lab05/Q2.c
+++ Lab05/Q2.c
@@ -5,18 +5,18 @@ int main(){
 	printf("Enter the number of weekday(1-7): ");
 	scanf("%d", &weekday);
 	printf("Enter if the user is on a vacation(1 for True and 0 for False): ");
-	scanf("%d", &weekday);
+	scanf("%d", &vacation);
 	
-	if (vacation==1)
+	if ((vacation==1) || (weekday>=6 && weekday<=7))
 	{
 		printf("Sleepin = True");
 	}
-	else if (weekday>=6 && weekday<=7)
-	{
-		printf("Sleepin = True");
-	}
-	else
+	else if ((vacation==0) || (weekday>=1 && weekday<=5))
 	{
 		printf("Sleepin = False");
+	}
+	else 
+	{
+	    printf("Error");
 	}
 }
