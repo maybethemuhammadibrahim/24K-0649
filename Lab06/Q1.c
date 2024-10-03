2ee9f56cb70c0acbb103cc454fac440e43189c02
Muhammad Ibrahim <theibraheemmuhammad@gmail.com>
2024-10-03 21:22:50 +0500

    Update Q1.c

--- Lab06/Q1.c
+++ Lab06/Q1.c
@@ -1,15 +1,20 @@
-/*While*/
+/*The type of loop to be used for user-input depends on the situation. 
+In this particular code we are using a do-while loop because we want 
+to run the loop atleast once for user-input and then decide wether we
+want the program to continue or not */
 
 #include<stdio.h>
-int main(){
-    int input=0, sum=0;
 
-    while(input!=0)
+int main(){
+    int input, sum;
+    input = 0;
+    
+    do
     {
         printf("Enter a Number: ");
         scanf("%d",&input);
         sum = sum+input;
-    }
+    }while(input != 0);
 
     printf("Your sum is: %d",sum);
 
