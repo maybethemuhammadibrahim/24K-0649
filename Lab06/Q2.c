f6a5360b621ee7784e8ec10d3dc8f0ad39ae20f5
Muhammad Ibrahim <theibraheemmuhammad@gmail.com>
2024-10-09 22:22:33 +0500

    Update Q2.c

--- Lab06/Q2.c
+++ Lab06/Q2.c
@@ -1,10 +1,23 @@
-/*While*/
-
 #include<stdio.h>
-int main(){
-    
-    for(int a=1;a<=5;a++){
-        printf("%d %d %d %d %d\n",a,a,a,a,a);
-    }
 
+int main(){
+    int number;
+    printf("Enter a number:");
+    scanf(" %d",&number);
+    
+    if(number==1){
+        printf("%d is neither composite nor prime.",number);
+        return 0;
+         }
+    if(number==2){
+        printf("%d is prime",number);
+    }
+  
+    for(int i=2;i<number;i++){
+        if(number%i == 0){
+            printf("%d is not prime because it is divisble by %d",number,i);
+            break;
+        }
+    }
+    printf("%d is Prime", number);
 }
