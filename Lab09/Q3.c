41c4bfed4a42cb5c2295774f1a690e660aa948b3
Muhammad Ibrahim <theibraheemmuhammad@gmail.com>
2024-11-09 21:48:00 +0500

    Update Q3.c

--- Lab09/Q3.c
+++ Lab09/Q3.c
@@ -1,33 +1,32 @@
-/*Create a C program that swaps the values of two integers using a user-defined function,
-swapIntegers. The user inputs two integer values, and the program uses the function to swap
-them. It should perform the swap and display the updated values.*/
-
 #include <stdio.h>
 
-int isPrime(int number){
-    int returnBool;
-    for(int i=2;i<number;i++){
-        if(number%i == 0){
-            returnBool = 0;
-            break;
-        }
-        else{
-            returnBool =1;
+int isPrime(int number) {
+    if (number <= 1) 
+        return 0;
+    if (number == 2) 
+        return 1;
+
+    for (int i = 2; i < number; i++) {
+        if (number % i == 0) {
+            return 0; 
         }
     }
-    return returnBool;
+    return 1; 
 }
 
-int main(){
+int main() {
     int number;
     printf("Enter the number to check: ");
-    scanf("%d",&number);
-    if(number==0){
-        printf("Zero is neither prime nor composite");
+    scanf("%d", &number);
+
+    if (number == 0) {
+        printf("Zero is neither prime nor composite\n");
         return 0;
     }
-    
+
     int result = isPrime(number);
-    result==1?printf("Number is Prime"):printf("Number is not Prime");
+    result == 1 ? printf("Number is Prime\n") : printf("Number is not Prime\n");
+
+    return 0;
 }
 
