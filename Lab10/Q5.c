51d4b1e53849fd5e2df134c24463c18b5dea1653
Muhammad Ibrahim <mibrahimfiftysix@gmail.com>
2024-11-22 12:57:09 +0500

    Update Q5.c

--- Lab10/Q5.c
+++ Lab10/Q5.c
@@ -4,32 +4,26 @@
 
 #include <stdio.h>
 
-void bubbleSort(int arr[],int n,int i){
-    static int sorted = 0, counter = 0;
-    int temp;
-    if(counter==n-1){
-        return ;
-    }
-    else{
+void bubbleSort(int arr[],int n){
+    // if(k<n){
+
+    for(int i=0;i<n;i++){
         if(arr[i]>arr[i+1]){
-            temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] =temp;
-            bubbleSort(arr,n,i+1);
-            printf("sorting\n");
-            counter = 0;
-        }
-        else{
-            bubbleSort(arr,n,i+1);
-            counter++;
+            int temp = arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+            bubbleSort(arr,n);
         }
     }
 }
 
+    
+
+
 int main(){
-    int arr[] = {2,3,2,5,2};
+    int arr[] = {5,3,2,5,2};
     int size = 5;//sizeof(arr)/sizeof(arr[0]);
-    bubbleSort(arr,size,0);
+    bubbleSort(arr,size);
     for(int i=0; i<5 ; i++){
         printf("%d",arr[i]);
     }
