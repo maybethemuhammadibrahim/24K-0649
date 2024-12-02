9e7d595481e6ce1099acc9c48b3307debce7139d
Muhammad Ibrahim <mibrahimfiftysix@gmail.com>
2024-12-02 16:50:51 +0500

    Update Q3.c

--- Lab11/Q3.c
+++ Lab11/Q3.c
@@ -10,32 +10,38 @@ int getLen(char *pointer){
     return i;
 }
 
-int main(){
-    FILE *file;
-    file = fopen("file1.txt", "r+");
-    if(file == NULL){
-        printf("FILE NOT READ PROPERLY");
-    }
-    char word[10],word2[10];
+void replace(FILE *file,char *find, char *word){
     char buffer[10];
     int length=0;
-    printf("Enter the word you want to replace in the file(max 10 char): ");
-    scanf("%s",word);
-    printf("What do you want to replace");
-    scanf("%s",word2);
-    
     while(fscanf(file,"%s", buffer) != EOF){
         // printf("%s",buffer);
         length = getLen(buffer)+length+1;
-        if(!strcmp(buffer, word)){
+        if(!strcmp(buffer, find)){
             fseek(file, length-(getLen(buffer)+1), SEEK_SET);
-            fprintf(file, "%s" , word2);
+            fprintf(file, "%s" , word);
 
         }
 
 
     }
     printf("Done");
+}
+
+int main(){
+    FILE *file;
+    file = fopen("file1.txt", "r+");
+    if(file == NULL){
+        printf("FILE NOT READ PROPERLY");
+    }
+    char find[10],word[10];
+    
+    printf("Enter the word you want to replace in the file(max 10 char): ");
+    scanf("%s",find);
+    printf("What do you want to replace in its place: ");
+    scanf("%s",word);
+    
+    replace(file, find, word);
+
     fclose(file);
     return 0;
 }
